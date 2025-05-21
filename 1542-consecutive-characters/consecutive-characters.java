79572c4732e9966bf2e9ff7a97c9e9cf123d80cc
Lokesh K <94343329+lokeshhh-10@users.noreply.github.com>
2025-05-21 21:32:29 +0530

    Time: 3 ms (4.93%) | Memory: 42.1 MB (76.23%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.java
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -5,18 +5,21 @@ class Solution {
         int max = 0;
         int sum = 0;
 
-        while (r < s.length()) {
-            if (s.charAt(l) == s.charAt(r)) {
-                sum += 1;
-                max = Math.max(max, sum);
-                r += 1;
-            } else {
-                sum = 1;  // Start new count
-                l = r;    // Move left pointer to new group
-                r += 1;   // Move right pointer
+        while(r<s.length()){
+            if(s.charAt(l) == s.charAt(r)){
+                sum+=1;
+                max = Math.max(sum,max);
+                r+=1;
+            }else{
+                sum = 1;
+                l = r;
+                r+=1;
             }
         }
 
         return max;
+        
+
+        
     }
 }
